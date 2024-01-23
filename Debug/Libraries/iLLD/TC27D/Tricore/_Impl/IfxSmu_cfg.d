b70164cd218f7519f283ca5b2c1e4391feb871d6
AndreasRichie <reichenauer.andreas@gmail.com>
2024-01-23 12:33:31 +0100

    remove debug folder

--- Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-c:/Users/loltr/OneDrive/Documents/FH Technikum Wien/Embedded Systems/MES I/Software/ES_SW_Projekt/MES_SW_Project2_Fitzko_Reichenauer_Stifter/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.c
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.c :
-c:/Users/loltr/OneDrive/Documents/FH Technikum Wien/Embedded Systems/MES I/Software/ES_SW_Projekt/MES_SW_Project2_Fitzko_Reichenauer_Stifter/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.h
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSmu_cfg.h :
