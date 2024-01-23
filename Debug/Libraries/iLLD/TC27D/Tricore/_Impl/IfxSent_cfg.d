e11c65b0f6c68c069de7fc93e30765a8c2975153
AndreasRichie <reichenauer.andreas@gmail.com>
2024-01-23 12:32:52 +0100

    remove debug

--- Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-f:/Dokumente/FH/ES Software/Multicore/workspace/MES_SW_Project2_Fitzko_Reichenauer_Stifter/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.c
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.c :
-f:/Dokumente/FH/ES Software/Multicore/workspace/MES_SW_Project2_Fitzko_Reichenauer_Stifter/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.h
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSent_cfg.h :
--- Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.d
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-f:/Dokumente/FH/ES Software/Multicore/workspace/MES_SW_Project2_Fitzko_Reichenauer_Stifter/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.c
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.c :
-f:/Dokumente/FH/ES Software/Multicore/workspace/MES_SW_Project2_Fitzko_Reichenauer_Stifter/Debug/Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.h
-../Libraries/iLLD/TC27D/Tricore/_Impl/IfxSrc_cfg.h :
